80af9a996819595280340b80b75caf862a9f5091
abhay660 <49646552+abhay660@users.noreply.github.com>
2019-05-06 20:37:46 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<input type="button" value="LED 1" onclick="location='test.php'" />
+<input type="button" value="LED 2" onclick="location='test1.php'" />
+<input type="button" value="LED 3" onclick="location='test2.php'" />
+<input type="button" value="LED 4" onclick="location='test3.php'" />
+<input type="button" value="LED 5" onclick="location='test4.php'" />
+<input type="button" value="LED 6" onclick="location='test5.php'" />
+<input type="button" value="LED 7" onclick="location='test6.php'" />
+<input type="button" value="LED 8" onclick="location='test7.php'" />
+        
